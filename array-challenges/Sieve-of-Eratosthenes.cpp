2faba4102eb0799ea5a53e79bd02548da9b3d49f
PranshuSharma1 <pranshu.sharma1996.ps@gmail.com>
2021-06-14 17:09:20 +0530

    Sieve-of-Eratosthenes.cpp

---
+++ array-challenges/Sieve-of-Eratosthenes.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+void sieve(int n){
+
+    int s[100]={0};
+
+    for (int i=2;i<+n;i++){
+
+        if(s[i]==0){
+            for(int j=i*i; j<=n; j+=i){
+                s[j]=1;
+            }
+        }
+    }
+
+    for(int i=2;i<=n;i++){
+        if(s[i]==0){
+            cout<<i<<" , ";
+        }
+    }cout<<endl;
+
+}
+
+int main(){
+
+    int n;
+    cout<<"enter the number of primes u want till :-";
+    cin>>n;
+    sieve(n);
+
+
+    return 0;
+}
+
+/* 
+output of the program :-
+
+
+enter the number of primes u want till :-50 
+2 , 3 , 5 , 7 , 11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47 ,
+
+*/
